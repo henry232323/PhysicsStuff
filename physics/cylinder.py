f40d74c024e0ba3344bb8644e5aba41cc59e2f6a
Henry <henry@typheus.me>
2019-04-11 21:38:03 -0600

    Add files via upload

---
+++ physics/cylinder.py
@@ -0,0 +1,17 @@
+from physics import inertia
+
+
+class Cylinder(inertia.InertiaObject):
+    def __init__(self, x, y, mass, radius, hollow=True, thickness=0):
+        moi = self.moment_of_inertia()
+        self.hollow = hollow
+        self.thickness = thickness
+        self.radius = radius
+        super().__init__(x, y, mass, moi)
+
+    def moment_of_inertia(self):
+        if self.hollow:
+            moi = .5 * self.mass * ((self.radius - self.thickness) ** 2 + self.radius ** 2)
+        else:
+            moi = .5 * self.mass * self.radius ** 2
+        return moi
---
+++ physics/inertia.py
@@ -0,0 +1,33 @@
+import math
+
+from physics import physics
+
+
+class InertiaObject(physics.PhysicsObject):
+    def __init__(self, x, y, mass, moi=None):
+        super().__init__(x, y, mass)
+        if moi is None:
+            self.inertia = self.moment_of_inertia()
+        else:
+            self.inertia = lambda: moi
+        self.mass = mass
+        self.angular_accel = 0
+        self.rotation = 0
+
+    def apply_force(self, fx, fy, x, y):
+        dist = math.hypot(self.x - x, self.y - y)
+        thetapos = math.atan2(self.x-x, self.y-y)
+        torque = math.hypot(fx, fy) * dist * math.sin(math.atan2(fx, fy) + thetapos)
+        radforce = math.hypot(fx, fy) * math.cos(math.atan2(fx, fy) + thetapos)
+
+        super().apply_force(radforce * math.cos(thetapos), radforce * math.sin(thetapos), x, y)
+
+        self.angular_accel += torque / self.inertia()
+
+    def move(self):
+        super().move()
+        self.rotation += self.angular_accel
+
+    def moment_of_inertia(self):
+        raise NotImplementedError("Implement this when you subclass, or pass the `moi` argument for a static"
+                                  " moment of inertia")
